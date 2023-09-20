2dbab3c4746347e405d0fd73ae91896d02174106
Vedran Nejedly <62423202+VedranNejedly@users.noreply.github.com>
2023-09-20 19:18:06 +0200

    Swiper downgrade for js to work

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-const  swiper = new Swiper(".swiper", {
-  // Optional parameters
-  loop: true,
-  effect: "fade",
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  scrollbar: {
-    el: ".swiper-scrollbar",
-  },
-});
-
-const  swiper2 = new Swiper(".swiper2", {
-  // Optional parameters
-  loop: true,
-  spaceBetween: 30,
-  breakpoints: {
-    600: {
-      slidesPerView: 1,
+  const swiper = new Swiper("#swiper1", {
+    // Optional parameters
+    loop: true,
+    effect: "fade",
+    autoplay:true,
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
     },
-    768: {
-      slidesPerView: 2,
+    scrollbar: {
+      el: ".swiper-scrollbar",
     },
-    992: {
-      slidesPerView: 3,
+  });
+  
+  const  swiper2 = new Swiper("#swiper2", {
+    // Optional parameters
+    loop: true,
+    spaceBetween: 30,
+    breakpoints: {
+      600: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      992: {
+        slidesPerView: 3,
+      },
     },
-  },
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination-2",
-    clickable: true,
-  },
-  scrollbar: {
-    el: ".swiper-scrollbar",
-  },
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination-2",
+      clickable: true,
+    },
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+  });
 });
-});
-
