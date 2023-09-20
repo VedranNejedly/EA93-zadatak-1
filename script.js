b854dc33f812d148090534a758d770268c6be9a9
Vedran Nejedly <62423202+VedranNejedly@users.noreply.github.com>
2023-09-20 17:40:35 +0200

    Swiper code moved to script.js adjusted container fluid on hero

---
+++ script.js
@@ -0,0 +1,38 @@
+const  swiper = new Swiper(".swiper", {
+    // Optional parameters
+    loop: true,
+    effect: "fade",
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+  });
+
+  const  swiper2 = new Swiper(".swiper2", {
+    // Optional parameters
+    loop: true,
+    spaceBetween: 30,
+    breakpoints: {
+      600: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      992: {
+        slidesPerView: 3,
+      },
+    },
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination-2",
+      clickable: true,
+    },
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+  });
